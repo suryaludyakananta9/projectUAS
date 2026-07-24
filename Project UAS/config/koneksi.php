6b941c4bbba6227693af9e937154f7d87ec092bb
suryaludyakananta9 <suryaludyakananta9@gmail.com>
2026-07-25 00:35:08 +0800

    Delete Project UAS directory

--- Project UAS/config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_toko_elektronik", [Gunakan port sesuai dengan DB Port anda]);
-
-if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
-
-mysqli_set_charset($koneksi, "utf8mb4");
-?>
